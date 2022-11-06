d089861aa960ccad5b50d8cf1bdd9b929d01c884
sherin j <58501547+sherinjohn58@users.noreply.github.com>
2022-11-06 12:31:44 +0530

    Portfolio

---
+++ assets/js/contact-fom.js
@@ -0,0 +1,204 @@
+$('form.contact-form-info').submit(function(e) {
+    e.preventDefault();
+    
+    var f = $(this).find('.form-group'),
+      ferror = false,
+      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+
+    f.children('input').each(function() { // run all inputs
+     
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'email':
+            if (!emailExp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'checked':
+            if (! i.is(':checked')) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'regexp':
+            exp = new RegExp(exp);
+            if (!exp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validate').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    f.children('textarea').each(function() { // run all inputs
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validate').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
+    if (ferror) return false;
+
+    var this_form = $(this);
+    var action = $(this).attr('action');
+
+    if( ! action ) {
+      this_form.find('.loading').slideUp();
+      this_form.find('.error-message').slideDown().html('The form action property is not set!');
+      return false;
+    }
+    
+    this_form.find('.sent-message').slideUp();
+    this_form.find('.error-message').slideUp();
+    this_form.find('.loading').slideDown();
+
+    if ( $(this).data('recaptcha-site-key') ) {
+      var recaptcha_site_key = $(this).data('recaptcha-site-key');
+      grecaptcha.ready(function() {
+        grecaptcha.execute(recaptcha_site_key, {action: 'php_email_form_submit'}).then(function(token) {
+          php_email_form_submit(this_form,action,this_form.serialize() + '&recaptcha-response=' + token);
+        });
+      });
+    } else {
+      php_email_form_submit(this_form,action,this_form.serialize());
+    }
+    
+    return true;
+  });
+
+  function php_email_form_submit(this_form, action, data) {
+    $.ajax({
+      type: "POST",
+      url: action,
+      data: data,
+      timeout: 500
+    }).done( function(msg){
+      if (msg == 'OK') {
+        this_form.find('.loading').slideUp();
+        this_form.find('.sent-message').slideDown();
+        this_form.find("input:not(input[type=submit]), textarea").val('');
+      } else {
+        this_form.find('.loading').slideUp();
+        if(!msg) {
+          msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
+        }
+        this_form.find('.error-message').slideDown().html(msg);
+      }
+    }).fail( function(data){
+      this_form.find('.loading').slideUp();
+        this_form.find('.sent-message').slideDown();
+        this_form.find("input:not(input[type=submit]), textarea").val('');
+      // console.log(data);
+      // var error_msg = "Form submission failed!<br>";
+      // if(data.statusText || data.status) {
+      //   error_msg += 'Status:';
+      //   if(data.statusText) {
+      //     error_msg += ' ' + data.statusText;
+      //   }
+      //   if(data.status) {
+      //     error_msg += ' ' + data.status;
+      //   }
+      //   error_msg += '<br>';
+      // }
+      // if(data.responseText) {
+      //   error_msg += data.responseText;
+      // }
+      // this_form.find('.loading').slideUp();
+      // this_form.find('.error-message').slideDown().html(error_msg);
+    });
+  }
+  
+if (ferror = false){
+var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  let contactInfo = firebase.database().ref("contact-fom");
+
+  // Listen for a submit
+  document.querySelector(".contact-form").addEventListener("submit", submitForm);
+  
+  function submitForm(e) {
+  e.preventDefault();
+  
+  //   Get input Values
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#email").value;
+  let subject = document.querySelector("#subject").value;
+  let message = document.querySelector("#message").value;
+  console.log(name, email, message);
+  
+  saveContactInfo(name, email, subject, message);
+  
+  document.querySelector(".contact-form").reset();
+  }
+  
+  // Save infos to Firebase
+  function saveContactInfo(name, email, subject, message) {
+  let newContactInfo = contactInfo.push();
+  
+  newContactInfo.set({
+    name: name,
+    email: email,
+    subject: subject,
+    message: message,
+  });
+  }
+}
---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').createServer(app);
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+// var emailjs = require('emailjs-com');
+
+const port = process.env.PORT || 3000;
+app.use('/assets',express.static('assets'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.locals.msg = '';
+    next();
+  });
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+app.get('/index.html', function(req,res){
+  res.sendFile(path.join(__dirname,'index.html'))
+})
+// app.post('/send', (req, res) => {
+//     let output = `
+//       <h1>New Email!</h1>
+//        <ul>
+//          <li>Name: ${req.body.name}</li>
+//          <li>Email: ${req.body.email}</li>
+//          <li>Subject: ${req.body.subject}</li>
+//          <li>Message: ${req.body.message}</li>
+//        </ul>
+//     `;
+  
+
+//     var templateParams = {
+//       Name: 'sherin'
+//       // ,
+//       // Email: `${req.body.email}`,
+//       // Subject: `${req.body.subject}`,
+//       // Message: `${req.body.message}`
+//   };
+   
+//   emailjs.send('', '', templateParams)
+//       .then(function(response) {
+//          console.log('SUCCESS!', response.status, response.text);
+//       }, function(error) {
+//          console.log('FAILED...', error);
+//       });
+    
+
+//     //   // create reusable transporter object using the default SMTP transport
+//     // let transporter = nodemailer.createTransport({
+//     //   service: 'gmail',
+//     //   host: 'smtp.gmail.com',
+//     //   port: 587,
+//     //   secure: true,
+//     //   auth: {
+//     //       user: '',
+//     //       pass: ''
+//     //   },
+//     //   tls:{
+//     //     rejectUnauthorized:false
+//     //   }
+//     // });
+  
+//     // // setup email data with unicode symbols
+//     // let mailOptions = {
+//     //     from: '"Nodemailer Contact" ', // sender address
+//     //     to: '', // list of receivers
+//     //     subject: 'Node Contact Request', // Subject line
+//     //     text: 'Hello world?', // plain text body
+//     //     html: output // html body
+//     // };
+  
+//     // // send mail with defined transport object
+//     // transporter.sendMail(mailOptions, (error, info) => {
+//     //     if (error) {
+//     //         return console.log(error);
+//     //     }
+//     //     console.log('Message sent: %s', info.messageId);
+//     //     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  
+//     //     res.render('contact', {msg:'Email has been sent'});
+//     // });
+//   });
+
+app.listen(port);
+console.log("Server Connected");
